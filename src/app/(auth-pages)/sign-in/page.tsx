29b202f410c98e353ae047a4908f4cc9024fac3e
haresh <hdpatel234@gmail.com>
2026-06-28 10:38:47 +0530

    feat: implement auth layout structure with side design and sign-in page routing

--- src/app/(auth-pages)/sign-in/page.tsx
+++ src/app/(auth-pages)/sign-in/page.tsx
@@ -1,7 +1,12 @@
 import SignInClient from './_components/SignInClient'
+import Side from '@/components/layouts/AuthLayout/Side'
 
 const Page = () => {
-    return <SignInClient />
+    return (
+        <Side>
+            <SignInClient />
+        </Side>
+    )
 }
 
 export default Page
